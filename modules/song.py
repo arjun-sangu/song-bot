94b3fc32ec9d1dcfb9585daca6ed788462d058ff
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2021-11-09 15:28:31 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -80,7 +80,7 @@ def a(client, message):
         message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
         m.delete()
     except Exception as e:
-        m.edit('**An internal Error Occured, Report This @redbullfed!!**')
+        m.edit('`An internal Error Occured..`\n**Send me a YouTube link with /song command 🙂**')
         print(e)
     try:
         os.remove(audio_file)
