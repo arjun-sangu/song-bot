058ad545965825af3af246f949737bf48fc72086
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2021-11-09 15:46:05 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -60,13 +60,13 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("`⏳Uploading Best quality song...`")
+    m.edit("**⏳Uploading Best quality song...**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>𝚃𝙸𝚃𝙻𝙴 :</b> <a href="{link}">{title}</a>\n⌚ <b>𝙳𝚄𝚁𝙰𝚃𝙸𝙾𝙽 :</b> <code>{duration}</code>\n📻 <b>𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝙳 :</b> <a href="https://t.me/MissHana_Msbot">ＨΛＮΛ</a>'
+        rep = f'🎶 <b>𝚃𝙸𝚃𝙻𝙴 :</b> <a href="{link}">{title}</a>\n\n⌚ <b>𝙳𝚄𝚁𝙰𝚃𝙸𝙾𝙽 :</b> <code>{duration}</code>\n📻 <b>𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝙳 :</b> <a href="https://t.me/MissHana_Msbot">ＨΛＮΛ</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
